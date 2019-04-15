39633b782ff805e230bc9f774efe406c2aa5fd5b
joye <shonubij@gmail.com>
2019-04-15 12:19:38 +0100

    added redux packages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+import {createStore} from "redux"
+import {Provider} from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import { searchRobots } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchRobots)
+ReactDOM.render(
+    <Provider store ={store}>
+    <App  />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
